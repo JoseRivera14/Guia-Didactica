78a1c7d2cae103c67e9315768d74864ffe683c9a
JoseRivera14 <jriveraguevara1403@gmail.com>
2025-10-12 23:07:20 -0600

    Commit Tercera Guia Jose Rivera

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,17 @@ async function bootstrap() {
     .setDescription('API description')
     .setVersion('1.0')
     .addTag('api')
+    .addBearerAuth({
+    type: 'http' ,
+    scheme: 'bearer',
+    bearerFormat: 'JWT',
+    in: 'header',
+    name: 'Authorization',
+    description: 'Enter your bearer token',
+})
+.addSecurityRequirements('bearer')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);

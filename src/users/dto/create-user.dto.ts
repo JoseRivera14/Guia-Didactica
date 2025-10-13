cc32f272cd55efaf60be02718a769dfc58e7ea25
JoseRivera14 <jriveraguevara1403@gmail.com>
2025-10-12 23:38:34 -0600

    Revert "Commit Tercera Guia Jose Rivera"
    This reverts commit 78a1c7d2cae103c67e9315768d74864ffe683c9a.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,12 @@
-
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto {
     @ApiProperty({required: true,example: 'usuario@empresa.com'})
 
     email: string;
-
     @ApiProperty({required: true,example: 'John Doe'})
     name: string;
 
-    
     username?: string;
 
     @ApiProperty({required: true,example: 'password123'})
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -14,7 +13,6 @@ export class UsersController {
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.usersService.findAll();
   }
